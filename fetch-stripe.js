8fb07d6df33975c1fa142ab6903d90db65c296b5
Jazmin Stewart <jazminstewart1234@gmail.com>
2026-07-26 21:36:17 +0100

    updating fetch file

--- fetch-stripe.js
+++ fetch-stripe.js
@@ -2,14 +2,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const fs = require('fs');
 
 async function update() {
-    const links = await stripe.paymentLinks.list({ active: true, expand: ['data.line_items.data.price.product'] });
+    const links = await stripe.paymentLinks.list({ active: true, expand: ['data.line_items.data.price'] });
     const data = {};
 
     for (const link of links.data) {
         if (!link.line_items || !link.line_items.data.length) continue;
         const lineItem = link.line_items.data[0];
         const price = lineItem.price;
-        const product = price.product;
+        const product = await stripe.products.retrieve(price.product);
 
         if (!product || !product.name) continue;
 
