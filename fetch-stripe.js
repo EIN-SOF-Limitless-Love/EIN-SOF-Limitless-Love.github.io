369904e1775a7a9c2db023ac0056b5b3a7038099
Jazmin Stewart <jazminstewart1234@gmail.com>
2026-07-26 20:59:05 +0100

    update fetch

--- fetch-stripe.js
+++ fetch-stripe.js
@@ -2,25 +2,27 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const fs = require('fs');
 
 async function update() {
-    const links = await stripe.paymentLinks.list({ active: true });
+    const links = await stripe.paymentLinks.list({ active: true, expand: ['data.line_items.data.price.product'] });
     const data = {};
 
     for (const link of links.data) {
-        const lineItems = await stripe.paymentLinks.listLineItems(link.id);
-        const price = await stripe.prices.retrieve(lineItems.data[0].price.id, { expand: ['product'] });
+        if (!link.line_items || !link.line_items.data.length) continue;
+        const lineItem = link.line_items.data[0];
+        const price = lineItem.price;
         const product = price.product;
 
-        const description = product.description || price.nickname || link.metadata?.description || '';
+        if (!product || !product.name) continue;
 
         data[product.name] = {
             url: link.url,
-            image: product.images[0] || '',
+            image: product.images && product.images[0] ? product.images[0] : '',
             price: price.unit_amount / 100,
             currency: price.currency,
-            description: description
+            description: product.description || ''
         };
     }
 
     fs.writeFileSync('data.json', JSON.stringify(data));
 }
+
 update();
