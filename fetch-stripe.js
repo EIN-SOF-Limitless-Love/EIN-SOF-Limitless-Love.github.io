81f50ae5b9236c94aac3fbd919a5dc685c5ffd75
Jazmin Stewart <jazminstewart1234@gmail.com>
2026-07-26 20:43:41 +0100

    updating fetch

--- fetch-stripe.js
+++ fetch-stripe.js
@@ -7,15 +7,17 @@ async function update() {
 
     for (const link of links.data) {
         const lineItems = await stripe.paymentLinks.listLineItems(link.id);
-        const price = await stripe.prices.retrieve(lineItems.data[0].price.id);
-        const product = await stripe.products.retrieve(price.product);
+        const price = await stripe.prices.retrieve(lineItems.data[0].price.id, { expand: ['product'] });
+        const product = price.product;
+
+        const description = product.description || price.nickname || link.metadata?.description || '';
 
         data[product.name] = {
             url: link.url,
             image: product.images[0] || '',
             price: price.unit_amount / 100,
             currency: price.currency,
-            description: product.description || ''
+            description: description
         };
     }
 
